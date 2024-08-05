5aaa466ed1b66ba78fc6f9654bc47c379a58b6e4
Noahthecoder09 <lunafuerst@gmx.de>
2024-08-05 21:11:35 +0200

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,7 @@
+document.getElementById("Yes").addEventListener("click", function() {
+    alert("Yayyy I like you too! <3");
+});
+
+document.getElementById("No").addEventListener("click", function() {
+    alert("Owww you don't?");
+});
